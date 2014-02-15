0de153189028aa60d560ae08fd08d45efc4e4c46
Eric Waring <ericw@the-feel.co.uk>
2014-02-15 13:36:50 +0000

    Initial Commit

---
+++ Surrealists/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Surrealists
+//
+//  Created by Eric Waring on 15/02/2014.
+//  Copyright (c) 2014 Eric Waring. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Surrealists/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Surrealists
+//
+//  Created by Eric Waring on 15/02/2014.
+//  Copyright (c) 2014 Eric Waring. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
